dd3dd8c010707dba9229476ff22b0444e7d88eb6
Benno Weber <bennopaul.weber@gmail.com>
2025-12-16 15:23:19 +0100

    before merge

--- trying_flutter/lib/main.dart
+++ trying_flutter/lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:mssql_connection/mssql_connection.dart';
+
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+
+
   const MyApp({super.key});
 
   // This widget is the root of your application.
