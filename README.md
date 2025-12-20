f8e574fb5347a2e8ade633857b80043caf6dd3a6
Dyumes <76482455+Dyumes@users.noreply.github.com>
2025-12-20 15:44:55 +0100

    Refactor, emulation for android, readme updated

--- README.md
+++ README.md
@@ -1,15 +1,149 @@
-TODO
+# Museum App 
+This project is done during our Software engineering and DataBase courses that we're following in the 2nd year of Computer Science Bachelor cursus from the University Of Applied Science of Sion, Valais, Switzerland. 
+## Description 
+### Context 
+A museum want a mobile-friendly app on which the *visitors* can scan an exhibit's QRCode, giving all the informations related to it. Furthermore, visitors should have access to thematic itineraries, scoring 1-5 stars per exhibits, add them to favorite and give a feedback. Their session has to be anonymized.
 
+The *admin* (Editors and curators) should have their own app with editing exhibits and having a dashboard for analytics, respectively.
 
-##how to start DB:
+This is a mobile-friendly app made on Flutter that works even with unreliable connectivity. There is 3 audiences simultaneously :
+1) **Visitor** : A lightweight app, giving details about museum's exhibits by scanning their corresponding QRCode ;
+2) **Editors** :  Edit, update, add new exhibit ;
+3) **Curator** : Analytics revealing visitor flows, dwell time, exhibit engagment and so on.
+
+## Docker and Database
+
+### How to start DB:
 1) open docker desktop
 2) Run docker compose up -d
 
-#if you want to recompose docker:
+### If you want to recompose docker:
 1) docker compose down -v !!do not do this if you do not want to delete ALL POSTGRES DATA
 2) docker compose up -d
 
-#start/stop docker:
+### Start/stop docker:
 - stop: docker compose stop
 - start: docker compose start
+
+
+## Prerequisites
+
+Before running the project, make sure you have installed and configured:
+
+* [Flutter](https://flutter.dev/docs/get-started/install) and added it to your `PATH`.
+* JDK with `JAVA_HOME` set.
+* Android SDK at `C:[YOUR PATH]\platformTools\AndroidSDK`.
+* ADB (`platform-tools`) accessible via `PATH`.
+* PostgreSQL DB works perfectly.
+
+---
+
+## Environment Variables
+
+Add to your PATH or verify:
+
+```powershell
+$env:ANDROID_SDK_ROOT="C:[YOUR PATH]\platformTools\AndroidSDK"
+$env:PATH += ";C:[YOUR PATH]\platformTools\AndroidSDK\platform-tools"
+```
+
+---
+
+## Android SDK Components
+
+Accept licenses:
+
+```powershell
+sdkmanager --licenses
+```
+
+Install required packages:
+
+```powershell
+sdkmanager "platform-tools" "cmdline-tools;latest" "system-images;android-33;google_apis;x86_64"
+```
+
+---
+
+## Create and Launch an AVD
+
+Create an Android Virtual Device (AVD) using Android Studio or command line.
+Example for Pixel 6 API 33:
+
+```powershell
+cd C:[YOUR PATH]\platformTools\AndroidSDK\emulator
+.\emulator.exe -avd Pixel_6_API_33
+```
+
+Check the emulator is recognized:
+
+```powershell
+adb devices
+```
+
+> If `adb` is not recognized, make sure `platform-tools` is in your `PATH`.
+
+---
+
+## Prepare the Flutter Project
+
+Navigate to the project folder:
+
+```powershell
+cd C:[YOUR PATH]\GenLog-DB-MuseumApp\trying_flutter
+```
+
+Install dependencies:
+
+```powershell
+flutter pub get
+```
+
+> Ensure DB settings are correct in `main.dart` if using PostgreSQL.
+
+---
+
+## Run the Application
+
+With the emulator already running:
+
+```powershell
+flutter run
+```
+
+* Hot reload: `r`
+* Hot restart: `R`
+
+---
+
+## Checks
+
+* APK builds and installs on the emulator.
+* App launches without connection errors.
+* Check logs for correct operation
+
+
+## Flutter
+### Installation
+1) Install Flutter SDK : https://flutter.dev/docs/get-started/install
+2) Add Flutter to the `PATH`
+3) Verify installation : run `flutter doctor`
+
+### Visual Studio Code 
+Install Flutter and Dart plugins.
+
+### Android SDK
+1) Install via Android Studio or standalone
+2) Install 
+
+### Android emulator 
+
+Has to be already done :
+1) **Flutter** installed and configured ( run `flutter doctor` should not display any errors) ;
+2) **Android SDK** installed (`platform-tools`, `cmdline-tools` and `emulator` included) ;
+3) **ADB** accessible from the terminal (add `platform-tools` to the PATH) ;
+4) **AVD (Android Virtual Device)** configured in Android SDK
+
+#### Launch emulator
+
 
--- trying_flutter/lib/main.dart
+++ trying_flutter/lib/main.dart
@@ -1,158 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:postgres/postgres.dart';
-import 'package:flutter/foundation.dart';
-
-late PostgreSQLConnection connection;
+import 'package:trying_flutter/data/exhibit_dao.dart';
+import 'package:trying_flutter/ui/pages/my_home_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  connection = PostgreSQLConnection(
-    'localhost',
+  final connection = PostgreSQLConnection(
+    'localhost', //for emulation : '10.0.2.2' / for physical device : 'localhost'
     5432,
     'museum_DB',
     username: 'admin',
     password: 'eaeaoh',
   );
 
-  try {
-    await connection.open();
-    print('Postgres: connection opened');
-  } catch (e, st) {
-    print('Postgres: connection failed -> $e');
-    print(st);
-  }
+  await connection.open();
 
-  runApp(const MyApp());
+  final exhibitDao = ExhibitDao(connection);
+
+  runApp(MyApp(exhibitDao: exhibitDao));
 }
 
 class MyApp extends StatelessWidget {
+  final ExhibitDao exhibitDao;
 
+  const MyApp({super.key, required this.exhibitDao});
 
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  Future<void> _incrementCounter() async {
-    // Run the async DB query outside setState
-    try {
-      var result = await connection.query(
-        'INSERT INTO exhibits(title, start_date, final_date, branding) VALUES (@title, @start_date, @final_date, @branding)',
-        substitutionValues: {
-          'title': 'ALLAN LE GRAND PIED',
-          'start_date': '2024-06-01',
-          'final_date': '2024-12-31',
-          'branding': 'Allan le grand pied est une exposition sur les pieds.',
-        },
-      );
-      print('Query result: $result');
-    } catch (e, st) {
-      print('Query failed: $e');
-      print(st);
-    }
-
-    // Update UI state synchronously
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      home: MyHomePage(
+        title: 'Flutter Demo Home Page',
+        exhibitDao: exhibitDao,
       ),
     );
   }
